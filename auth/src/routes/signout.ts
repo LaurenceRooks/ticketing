94d6a5dce8083196d080c922c2b2e55a4a863d0f
Laurence Rooks <rooks.laurence@icloud.com>
2022-08-05 07:46:38 -0500

    Completed section 9 for authorization.

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-    res.send('Hi there!');
+    // Set the session to null to remove everything from the cookie session and sign the user out.
+    req.session = null;
+    
+    // Return an empty object to confirm the session is null.
+    res.send({});
 });
 
 export { router as signoutRouter };
